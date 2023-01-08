41944be58c10836bdbf71e212bb62e16b60a4e23
vicx0 <121810791+vicx0@users.noreply.github.com>
2023-01-08 03:24:53 +0100

    Update update-log.lua

--- update-log.lua
+++ update-log.lua
@@ -4,5 +4,5 @@
                             <  Update Log  >
        
                    [+] Last update in 00:29 xd
-                   [+]
+                   [+] blah blah blah updates every 5 minutes i think
                    [+]
