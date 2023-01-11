e2034d2f68f3968bd11578e2d2d001c91879f9eb
vicx0 <121810791+vicx0@users.noreply.github.com>
2023-01-11 21:19:00 +0100

    Update update-log.lua

--- update-log.lua
+++ update-log.lua
@@ -2,7 +2,11 @@
 
 
                             <  Update Log  >
-       
-                   [+] Last update in 00:29 xd
-                   [+] blah blah blah updates every 5 minutes i think
-                   [+] lmaoooo
+                                  V1.11
+               [/] Global remake Antiaim
+               [/] Remade Visuals
+               [/] Remaked Fakelag modes and its better now!
+               [+] Added Watermark Themes (Blue, Red, Green Yellow)
+               [+] Added Indicators Themes (Blue, Red, Green Yellow)
+               [+] Added More antiaims Presets to use!
+                   -- THIS VERSION ISN'T RELEASED YET --
