9db4fdcc092fdb02b5098662147cf66af6177be3
vicx0 <121810791+vicx0@users.noreply.github.com>
2023-01-11 21:21:56 +0100

    Update update-log.lua

--- update-log.lua
+++ update-log.lua
@@ -6,7 +6,13 @@
                [/] Global remake Antiaim
                [/] Remade Visuals
                [/] Remaked Fakelag modes and its better now!
-               [+] Added Watermark Themes (Blue, Red, Green Yellow)
-               [+] Added Indicators Themes (Blue, Red, Green Yellow)
+               [+] Added Watermark Themes (Blue, Red, Green, Yellow)
+               [+] Added Watermark mode: Classic + RGB line!
+               [+] Added Indicators Themes (Blue, Red, Green, Yellow)
                [+] Added More antiaims Presets to use!
                    -- THIS VERSION ISN'T RELEASED YET --
+                           IF YOU AREN'T USING
+                                   LOADER
+                      THERE WON'T BE AUTO DOWNLOAD OF
+                                PARANOIA LUA
+  
