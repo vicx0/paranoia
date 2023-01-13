3061aaa112834ad56eef34f8f641b613d0489c13
vicx0 <121810791+vicx0@users.noreply.github.com>
2023-01-13 18:05:41 +0100

    Update update-link.lua

--- update-link.lua
+++ update-link.lua
@@ -1,1 +1,1 @@
-https://cdn.discordapp.com/attachments/1061379637252460654/1063480642349830214/PARANOIA.plua
+https://cdn.discordapp.com/attachments/1061379637252460654/1063503643174764615/PARANOIA.plua
