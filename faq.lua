373821229017f560f79377ab58b4571a4a94878a
vicx0 <121810791+vicx0@users.noreply.github.com>
2023-01-14 15:33:10 +0100

    Create faq.lua

---
+++ faq.lua
@@ -0,0 +1,10 @@
+      Make sure you have "PARANOIA" folder in disk C:\
+      Make sure you are using latest loader!
+      LOADER is AutoDownloader From HTTPS and it just skip
+      DOWNLOADING by urself the script!
+  
+      ____________________________________________________
+      CODED BY: vicx#6006  / If you readed above
+      And did folder; then contact with me at discord
+      ____________________________________________________
+                       PARANOIA LOADER
