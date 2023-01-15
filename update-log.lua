58c66145a4d67c0cb37447b045a3994765ec6967
vicx0 <121810791+vicx0@users.noreply.github.com>
2023-01-15 12:09:26 +0100

    Update update-log.lua

--- update-log.lua
+++ update-log.lua
@@ -3,13 +3,7 @@
 
                             <  Update Log  >
                                   V1.12
-               [+] Added Menu management !
-               [+] Added more watermark options / also for indicators
-               [+] Recoded some functions!
-               [-] deleted modern indicator & watermark
+               [!] IN FEW DAYS THERE'S GONNA BE NEWER VERSION
+               [!] BE PATIENT
                    -- THIS VERSION ISN'T RELEASED YET --
-                           IF YOU AREN'T USING
-                                   LOADER
-                      THERE WON'T BE AUTO DOWNLOAD OF
-                                PARANOIA LUA
-  
+                            NEWER LOADER IS DONE!
